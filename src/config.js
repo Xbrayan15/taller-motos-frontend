3a888376fa1c91f99e24a3d696b9a7d5cff220c8
Brayan Alexánder Martínez Zambrano <jhermoso2005@gmail.com>
2026-04-17 16:00:23 -0500

    backend url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // Configuración de la aplicación
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://taller-moto-pavas.onrender.com';
 
 export const CONFIG = {
   API_BASE_URL,
